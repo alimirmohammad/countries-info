f5ba34ed77779480d3faa34f5e175d24693e9ec2
Ali Mirmohammad <ali.mirmohammad.86@gmail.com>
2022-08-18 10:22:59 +0430

    fix: fix pinia bug in production for syncing query params

--- src/utils/store-subscribe.ts
+++ src/utils/store-subscribe.ts
@@ -1,19 +1,27 @@
+import { computed, watch } from "vue";
+
 import type { FilterData } from "@/typings/dto";
 import router from "@/router";
 import { useCountriesStore } from "@/stores/countries";
 
 export default function subscribeToStore() {
-  useCountriesStore().$subscribe((mutation, state) => {
-    const mutationKey = Array.isArray(mutation.events)
-      ? mutation.events.map((m) => m.key).join(", ")
-      : mutation.events.key;
-
-    if (["region", "query", "sortBy"].includes(mutationKey)) {
+  const store = useCountriesStore();
+  const data = computed(() => ({
+    region: store.region,
+    query: store.query,
+    sortBy: store.sortBy,
+  }));
+  watch(data, (newValue, oldValue) => {
+    if (
+      newValue.region !== oldValue.region ||
+      newValue.query !== oldValue.query ||
+      newValue.sortBy !== oldValue.sortBy
+    ) {
       const query: Partial<FilterData> = {};
 
-      if (state.region) query.region = state.region;
-      if (state.query) query.query = state.query;
-      if (state.sortBy) query.sortBy = state.sortBy;
+      if (store.region) query.region = store.region;
+      if (store.query) query.query = store.query;
+      if (store.sortBy) query.sortBy = store.sortBy;
 
       router.replace({
         name: "home",
